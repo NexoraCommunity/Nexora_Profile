3863159042f7884ad8830d55e3e5b7d02bea73d8
Ifqii <rifqi17faiza@gmail.com>
2025-07-22 11:21:39 +0700

    push gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
-'use client'
 import Navbar from '@/components/Navbar'
 import About from '@/Features/Homepage/About'
+import Gallery from '@/Features/Homepage/Gallery'
 import HeroSection from '@/Features/Homepage/HeroSection'
 import Portfolio from '@/Features/Homepage/Portfolio'
 import React, { useState } from 'react'
 
 const page = () => {
   return (
-    <div className=' w-full bg-white'>
+    <div className=' w-full bg-white text-white'>
       <Navbar />
       <HeroSection />
       <About />
       <Portfolio />
+      <Gallery />
     </div>
   )
 }
