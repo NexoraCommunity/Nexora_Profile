2c91bdd55670b04e3295301be3cab469b73d7716
Pilipus <Pilipus@gmail.com>
2025-07-26 15:51:52 +0700

    feat: add interactive add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,37 +7,40 @@ import { IoIosArrowDown } from 'react-icons/io'
 const Navbar = () => {
     const [Toggle, SetToggle] = useState(false)
     return (
-        <nav className='nav fixed flex justify-center w-full '>
-            <div className='w-11/12  top-0 py-6  text-white flex justify-between items-center text-3xl mx-auto'>
-                <div className='flex gap-3 items-center w-96 max-lg:hidden'>
-                    <Link href={'#home'} className='p-2 px-6 bg-white/10 backdrop-blur-lg rounded-3xl'>HOME</Link>
-                    <Link href={'#about'} className='p-2 px-6 bg-white/10 backdrop-blur-lg  rounded-3xl'>TENTANG KAMI</Link>
+        <nav className='nav fixed flex justify-center w-full'>
+            <div className='w-11/12  top-0 py-6  text-white  z-10 text-3xl  flex justify-between items-start mx-auto'>
+                <div className={`flex justify-between relative  flex-grow  items-center duration-300`}>
+                    <div className='flex gap-3 items-start w-96 max-lg:hidden'>
+                        <Link href={'#home'} className='p-2 px-6 bg-white/10 backdrop-blur-lg rounded-3xl'>HOME</Link>
+                        <Link href={'#about'} className='p-2 px-6 bg-white/10 backdrop-blur-lg  rounded-3xl'>TENTANG KAMI</Link>
+                    </div>
+                    <h1 className='mx-20'>NEXORA<span className='text-[#56DFCF] '>.</span></h1>
+                    <div className='flex gap-3 items-start w-96 max-lg:hidden'>
+                        <Link href={'#faq'} className='p-2 px-6 bg-white/10 backdrop-blur-lg  rounded-3xl'>FAQ</Link>
+                        <Link href={'#gallery'} className='p-2 px-6 bg-white/10 backdrop-blur-lg  rounded-3xl'>GALLERY</Link>
+                    </div>
                 </div>
-                <h1>NEXORA<span className='text-[#56DFCF]'>.</span></h1>
-                <div className='flex gap-3 items-center w-96 max-lg:hidden'>
-                    <Link href={'#faq'} className='p-2 px-6 bg-white/10 backdrop-blur-lg  rounded-3xl'>FAQ</Link>
-                    <Link href={'#gallery'} className='p-2 px-6 bg-white/10 backdrop-blur-lg  rounded-3xl'>GALLERY</Link>
-                </div>
-            </div>
-            <div className='lg:w-96 right-0 w-full max-lg:top-0  absolute'>
-                <button onClick={() => SetToggle(!Toggle)} className='absolute mt-6 right-10'><Image src={"/hamburger.png"} alt='oke' width={50} height={50} /></button>
-
-                <div className={`${Toggle ? "" : "hidden"} max-lg:w-screen w-full bg-black h-auto  lg:pt-6 lg:right-5  absolute top-0 flex flex-col`}>
-                    <div className='w-full border relative flex items-center justify-center border-white text-4xl py-7'>
+                <div className={`${Toggle ? "scale-x-100 lg:w-96 w-full" : "w-0 h-0"} ease-in-out lg:relative absolute scale-x-0  duration-300 origin-right  lg:-right-5 max-lg:left-0 top-0 z-10    flex flex-col`}>
+                    <div className='w-full border relative flex items-center bg-black justify-center border-white text-5xl py-8'>
                         <h1>More</h1>
-                        <button onClick={() => SetToggle(!Toggle)} className='absolute right-5'><IoIosArrowDown /></button>
+                        <button onClick={() => SetToggle(!Toggle)} className='absolute right-5'><IoIosArrowDown className={`${Toggle ? "rotate-0" : "rotate-180"} duration-300`} /></button>
                     </div>
-                    <div className='xl:hidden flex flex-col'>
-                        <Link href={'#homr'} className='w-full border text-center border-white text-xl py-3'>Home</Link>
-                        <Link href={'#about'} className='w-full border text-center border-white text-xl py-3'>Tentang Kami</Link>
-                        <Link href={'#faq'} className='w-full border text-center border-white text-xl py-3'>Faq</Link>
-                        <Link href={'#gallery'} className='w-full border text-center border-white text-xl py-3'>Gallery</Link>
+                    <div className={`${Toggle ? "scale-y-100" : "scale-y-0 "}  ease-in-out bg-black flex flex-col  origin-top duration-500`}>
+                        <div className='xl:hidden flex flex-col'>
+                            <Link href={'#homr'} className='w-full border text-center border-white text-2xl py-6'>Home</Link>
+                            <Link href={'#about'} className='w-full border text-center border-white text-2xl py-6'>Tentang Kami</Link>
+                            <Link href={'#faq'} className='w-full border text-center border-white text-2xl py-6'>Faq</Link>
+                            <Link href={'#gallery'} className='w-full border text-center border-white text-2xl py-6'>Gallery</Link>
+                        </div>
+                        <Link href={'#portfolio'} className='w-full border text-center border-white text-2xl py-6'>Portfolio</Link>
+                        <Link href={'#divisi'} className='w-full border text-center border-white text-2xl py-6'>Divisi</Link>
+                        <Link href={'#kontakkami'} className='w-full border text-center border-white text-2xl py-6'>Kontak Kami</Link>
                     </div>
-                    <Link href={'#portfolio'} className='w-full border text-center border-white text-xl py-3'>Portfolio</Link>
-                    <Link href={'#divisi'} className='w-full border text-center border-white text-xl py-3'>Divisi</Link>
-                    <Link href={'#kontakkami'} className='w-full border text-center border-white text-xl py-3'>Kontak Kami</Link>
                 </div>
 
+                <div className='absolute mt-6 right-10 z-0 top-0'>
+                    <button onClick={() => SetToggle(!Toggle)}><Image src={"/hamburger.png"} alt='oke' width={50} height={50} /></button>
+                </div>
             </div>
 
         </nav>
